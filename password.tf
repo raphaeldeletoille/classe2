f1ce2484a370f25ab3fad2651fd19ae65fbed585
raphaeldeletoille <58251038+raphaeldeletoille@users.noreply.github.com>
2024-06-07 18:29:52 +0200

    gl hf

--- password.tf
+++
@@ -1,8 +0,0 @@
-resource "random_password" "password" {
-  count            = 2
-  length           = 26
-  special          = true
-  upper            = true
-  min_upper        = 1
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
