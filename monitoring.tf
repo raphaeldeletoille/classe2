b450ea259e714da4e90bd1476f489b7c297028cd
raphaeldeletoille <58251038+raphaeldeletoille@users.noreply.github.com>
2024-06-07 17:53:33 +0200

    add grafana

--- monitoring.tf
+++ monitoring.tf
@@ -37,6 +37,34 @@ resource "azurerm_monitor_diagnostic_setting" "rule" {
   }
 }
 
+data "azurerm_subscription" "primary" {
+}
+
+resource "azurerm_dashboard_grafana" "grafana" {
+  name                              = "raph-grafana"
+  resource_group_name               = azurerm_resource_group.rg.name
+  location                          = "West Europe"
+  api_key_enabled                   = true
+  deterministic_outbound_ip_enabled = true
+  public_network_access_enabled     = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+resource "azurerm_role_assignment" "grafana_permission" {
+  role_definition_name = "Monitoring Reader"
+  scope = data.azurerm_subscription.primary.id
+  principal_id = azurerm_dashboard_grafana.grafana.identity[0].principal_id
+}
+resource "azurerm_role_assignment" "my_user_permission" {
+  role_definition_name = "Grafana Admin"
+  scope = azurerm_dashboard_grafana.grafana.id 
+  principal_id = data.azurerm_client_config.current.object_id
+}
+
+User (classe2, Grafana Admin) --> Grafana (identité, Monitoring Reader) --> Logs & Metrics 
+
 #DEPLOYER GRAFANA (AZURERM_DASHBOARD_GRAFANA)
 #DONNER LE DROIT "Grafana Admin" à votre compte utilisateur sur votre grafana  (Role_Assignment)
 #DONNER LE DROIT "Monitoring Reader" à l'identité de votre Grafana sur votre souscription (Role_Assignment)
